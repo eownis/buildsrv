fb0d7fd63a3a9d06b20f025e814997542c8dd5eb
Gregor Noczinski <gregor@noczinski.eu>
2016-10-15 18:47:18 +0200

    Register github.com/echocat/caddy-filter for known plugins (#33)
    * * Register github.com/echocat/caddy-filter for known plugins
    
    * Improve the docs

--- features/registry.go
+++ features/registry.go
@@ -168,6 +168,13 @@ var Registry = Plugins{
 		Description: "Add expiration headers to assets",
 		DocsURL:     "/docs/expires",
 	},
+	{
+		Type:        DirectivePlugin,
+		Name:        "filter",
+		Import:      "github.com/echocat/caddy-filter",
+		Description: "Filter response body contents",
+		DocsURL:     "/docs/filter",
+	},
 
 	// DNS providers
 	{
